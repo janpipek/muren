3effd061db0b16b1b422455e64a86d794aa71e08
Jan Pipek <jan.pipek@gmail.com>
2024-04-11 18:31:10 +0200

    Fix bug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::{env, path::PathBuf, path::Path};
 use std::fs::rename;
+use std::{env, path::Path, path::PathBuf};
 
-use colored::Colorize;
 use clap::{arg, command, value_parser, Arg, ArgAction, Command};
-
+use colored::Colorize;
 
 fn ensure_extension_many(files: Vec<PathBuf>, extension: &String, dry: bool) {
     for entry in files {
@@ -12,21 +11,34 @@ fn ensure_extension_many(files: Vec<PathBuf>, extension: &String, dry: bool) {
 }
 
 fn ensure_extension_one(path: &PathBuf, extension: &String, dry: bool) {
-        let mut new_name = path.clone();
-        new_name.set_extension(extension);
-        maybe_rename(path, new_name.as_path(), dry);
+    let mut new_name = path.clone();
+    new_name.set_extension(extension);
+    maybe_rename(path, new_name.as_path(), dry);
 }
 
 fn maybe_rename(path: &Path, new_name: &Path, dry: bool) {
     if dry {
-        println!("- {0} → {1}", path.to_string_lossy().red(), new_name.to_string_lossy().green())
-    }
-    else {
+        println!(
+            "- {0} → {1}",
+            path.to_string_lossy().red(),
+            new_name.to_string_lossy().green()
+        )
+    } else {
         match rename(path, new_name) {
-            Ok(_) => println!("{0} {1} → {2}", "✓".green(), path.to_string_lossy().red(), new_name.to_string_lossy().green()),
-            Err(_) => eprintln!("{0} {1} → {2}", "✗".red(), path.to_string_lossy().red(), new_name.to_string_lossy().green()),
+            Ok(_) => println!(
+                "{0} {1} → {2}",
+                "✓".green(),
+                path.to_string_lossy().red(),
+                new_name.to_string_lossy().green()
+            ),
+            Err(_) => eprintln!(
+                "{0} {1} → {2}",
+                "✗".red(),
+                path.to_string_lossy().red(),
+                new_name.to_string_lossy().green()
+            ),
         }
-    }   
+    }
 }
 
 fn remove_string_many(files: Vec<PathBuf>, pattern: &String, dry: bool) {
@@ -39,57 +51,77 @@ fn remove_string_one(path: &Path, pattern: &String, dry: bool) {
     if path.to_string_lossy().contains(pattern) {
         let new_name = path.to_string_lossy().replace(pattern, "");
         maybe_rename(path, Path::new(&new_name), dry);
-    }
-    else {
+    } else {
         println!("= {0}", path.to_string_lossy());
     }
 }
 
-
 fn main() {
     let command = command!()
-        .arg(arg!(
-            -d --dry ... "Dry run"
-        ).global(true).action(clap::ArgAction::SetTrue))
+        .arg(
+            arg!(
+                -d --dry ... "Dry run"
+            )
+            .global(true)
+            .action(clap::ArgAction::SetTrue),
+        )
         .subcommand(
             Command::new("setext")
                 .about("Change extension")
                 .arg(
-                    Arg::new("extension").help("Extension to set (dot excluding)")
+                    Arg::new("extension")
+                        .help("Extension to set (dot excluding)")
                         .action(ArgAction::Set)
                         .value_parser(value_parser!(String))
-                )                
-                .arg(Arg::new("path").action(ArgAction::Append).value_parser(value_parser!(PathBuf)))
-
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("path")
+                        .action(ArgAction::Append)
+                        .value_parser(value_parser!(PathBuf)),
+                ),
         )
         .subcommand(
             Command::new("remove")
                 .about("Remove part of a name from all files.")
                 .arg(
-                    Arg::new("pattern").help("The string to remove").action(ArgAction::Set)
-                    .value_parser(value_parser!(String))
+                    Arg::new("pattern")
+                        .help("The string to remove")
+                        .action(ArgAction::Set)
+                        .value_parser(value_parser!(String))
+                        .required(true),
                 )
-                .arg(Arg::new("path").action(ArgAction::Append).value_parser(value_parser!(PathBuf)))
-        )
-        ;
+                .arg(
+                    Arg::new("path")
+                        .action(ArgAction::Append)
+                        .value_parser(value_parser!(PathBuf)),
+                ),
+        );
     let matches = command.get_matches();
 
     if let Some(matches) = matches.subcommand_matches("setext") {
         ensure_extension_many(
-            matches.get_many::<PathBuf>("path").unwrap().cloned().collect(), 
-            matches.get_one("extension").unwrap(), 
-            matches.get_flag("dry")
+            matches
+                .get_many::<PathBuf>("path")
+                .unwrap()
+                .cloned()
+                .collect(),
+            matches.get_one("extension").unwrap(),
+            matches.get_flag("dry"),
         );
     }
 
     if let Some(matches) = matches.subcommand_matches("remove") {
         remove_string_many(
-            matches.get_many::<PathBuf>("path").unwrap().cloned().collect(), 
+            matches
+                .get_many::<PathBuf>("path")
+                .unwrap()
+                .cloned()
+                .collect(),
             matches.get_one("pattern").unwrap(),
-            matches.get_flag("dry")
+            matches.get_flag("dry"),
         );
     }
-
 
     /* let mut args: Vec<String> = env::args().collect();
     args.remove(0); // The executable itself
