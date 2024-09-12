9cac1b6b503c416c6f17addd895ba9ed8052f484
Jan Pipek <jan.pipek@gmail.com>
2024-09-12 15:07:47 +0200

    Split modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+mod extensions;
+
 use colored::Colorize;
 use regex::Regex;
 use std::fmt::{Display, Formatter, Result};
 use std::fs::rename;
-use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
-use std::process::{self, exit};
+
+use crate::extensions::{find_extensions_from_content, has_correct_extension};
 
 extern crate unidecode;
 use unidecode::unidecode;
@@ -130,95 +132,15 @@ fn suggest_rename(path: &PathBuf, command: &RenameCommand) -> RenameIntent {
                 };
                 PathBuf::from(new_name)
             }
-        }}
+        },
+    }
 }
 
 fn suggest_renames(files: &[PathBuf], command: &RenameCommand) -> Vec<RenameIntent> {
     files
         .iter()
-        .map(|path| suggest_rename(
-            path,
-            command,
-        ))
+        .map(|path| suggest_rename(path, command))
         .collect()
-}
-
-fn infer_mimetype(path: &Path, mime_type: bool) -> Option<String> {
-    // TODO: Do something on windows :see_no_evil:
-    let mut cmd = process::Command::new("file");
-    let cmd_with_args = cmd.arg(path).arg("--brief");
-    let cmd_with_args = if mime_type {
-        cmd_with_args.arg("--mime-type")
-    } else {
-        cmd_with_args
-    };
-
-    let output = cmd_with_args.output();
-    match output {
-        Ok(output) => {
-            let output_str = String::from_utf8(output.stdout).unwrap();
-            let mime_type = match output_str.strip_suffix('\n') {
-                Some(s) => String::from(s),
-                None => output_str,
-            };
-            Some(mime_type)
-        }
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => {
-                eprintln!("Error: `file` probably not installed");
-                exit(-1);
-            }
-            _ => panic!("{e}"),
-        },
-    }
-}
-
-fn find_extensions_from_content(path: &Path) -> Vec<String> {
-    let mime_type_based = match infer_mimetype(path, true) {
-        None => vec![],
-        Some(mime_type) => {
-            let mime_type_str = mime_type.as_str();
-            match mime_type_str {
-                "application/pdf" => vec![String::from("pdf")],
-                "image/jpeg" => vec![String::from("jpeg"), String::from("jpg")],
-                "image/png" => vec![String::from("png")],
-                "text/csv" => vec![String::from("csv")],
-                "text/html" => vec![String::from("html"), String::from("htm")],
-                "text/x-script.python" => vec![String::from("py"), String::from("pyw")],
-                _other => vec![],
-            }
-        }
-    };
-
-    let mut description_based = match infer_mimetype(path, false) {
-        None => vec![],
-        Some(description) => {
-            let description_str = description.as_str();
-            match description_str {
-                "Apache Parquet" => vec![String::from("parquet"), String::from("pq")],
-                _other => vec![],
-            }
-        }
-    };
-
-    let mut extensions = mime_type_based.clone();
-    extensions.append(&mut description_based);
-    extensions
-}
-
-fn has_correct_extension(path: &Path, possible_extensions: &[String]) -> bool {
-    if possible_extensions.is_empty() {
-        true
-    } else {
-        let current_extension = path.extension();
-        if current_extension.is_none() {
-            false
-        } else {
-            let extension = current_extension.unwrap().to_ascii_lowercase();
-            let extension_str = String::from(extension.to_string_lossy());
-            possible_extensions.contains(&extension_str)
-        }
-    }
 }
 
 fn try_rename(path: &Path, new_name: &Path) -> bool {
